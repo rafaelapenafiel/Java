bf06cd402ff990fe26f9fe8e9aad14e30bc33297
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:30:25 -0300

    Create janela dialogo.java

---
+++ Exercícios/janela dialogo.java
@@ -0,0 +1,15 @@
+package aula;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Entre com um nome:");
+        if (nome!=null){
+            boolean name = nome.equals("")==false
+                    System.out.println(nome);
+        }
+    }
+        
+}
