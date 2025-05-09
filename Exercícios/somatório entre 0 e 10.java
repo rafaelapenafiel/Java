c7455e3e12a994c1e379ad70c547598f149eac6b
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:28:59 -0300

    Create somatório entre 0 e 10.java

---
+++ Exercícios/somatório entre 0 e 10.java
@@ -0,0 +1,13 @@
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int s = 0;
+        for (int i=0; i<=10; i++){
+           s += i;
+        }
+        System.out.println(s);
+    }
+        
+}
