12f455a51a8dcdbdd8a613791307a3e0ae99b185
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:26:43 -0300

    Create 1 a 5 while.java

---
+++ Exercícios/1 a 5 while.java
@@ -0,0 +1,13 @@
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i= 1;
+        while (i<=5) {
+        System.out.println(i);
+        i++;
+        }
+    }
+    
+}
