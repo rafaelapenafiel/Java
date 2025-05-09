06fcd98939fb6140d704c56a5ea78d869e21a10e
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:29:51 -0300

    Create média números ímpares 0 a 100.java

---
+++ Exercícios/média números ímpares 0 a 100.java
@@ -0,0 +1,15 @@
+public class Principal {
+    
+    public static void main(String[] args) {
+       int s = 0;
+       int contador = 0;
+       for (int i = 0; i<=100; i++){
+           if (i%2==1);
+                s+= i;
+                contador++;
+    }
+       double media = s / contador;
+       System.out.println(media);
+        
+    }
+}
