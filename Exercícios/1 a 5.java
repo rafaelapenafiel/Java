41e1cecac152dba3e4815d1395d869b32773b2f8
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:25:58 -0300

    Create 1 a 5.java

---
+++ Exercícios/1 a 5.java
@@ -0,0 +1,11 @@
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++) {
+        System.out.println(i);
+        }
+    }
+    
+}
