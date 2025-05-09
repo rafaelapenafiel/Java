13934cef0d482293fc8d2f8f357626cca5baaab7
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:28:05 -0300

    Create 1 a 15 pares.java

---
+++ Exercícios/1 a 15 pares.java
@@ -0,0 +1,15 @@
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        for (i=1; i<=15; i++) {
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+        
+    }
+    
+}
