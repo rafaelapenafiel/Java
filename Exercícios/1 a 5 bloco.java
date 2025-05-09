4393cc0999dcfe44cc5373b11a41fbdcfc4f51db
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 08:27:40 -0300

    Create 1 a 5 bloco.java

---
+++ Exercícios/1 a 5 bloco.java
@@ -0,0 +1,13 @@
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i= 1;
+        do {
+        System.out.println(i);
+        i=i+1;
+        } while (i<6);
+    }
+    
+}
